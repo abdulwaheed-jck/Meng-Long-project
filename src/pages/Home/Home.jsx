457751519e8da72b29231fac4a125258c52c022e
abduvohid-jck <abduvohidmurodov26@gmail.com>
2025-08-28 13:52:20 +0500

    Language select menu added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import Navbar from "../../sections/Navbar";
-import HeroBanner from "../../assets/HeroBanner.png";
+import Thread from "../../assets/Thread.png";
 function Home() {
   return (
     <div className="min-w-[400px]">
       <Navbar />
-      <div className="w-[100%] lg:max-w-[1300px] mx-auto mt-[12px]">
-        <img
-          className="max-h-[619px] w-[100%] object-cover"
-          src={HeroBanner}
-          alt="HeroBanner"
-        />
-      </div>
+      
     </div>
   );
 }
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -4,7 +4,18 @@ import Logo from "../assets/LogoForWebsite.png";
 function Navbar() {
   return (
     <div className="pb-[15px] pt-[15px] border-b-[3px] border-b-solid border-b-[#632393] shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-      <img className="h-[55px] lg:h-[65px] m-auto" src={Logo} alt="Logo" />
+      <div className="max-w-[1280px] m-auto flex items-center relative">
+        <img className="h-[55px] lg:h-[65px] m-auto" src={Logo} alt="Logo" />
+        <select
+          name="languages"
+          id="languages"
+          className="absolute right-[15px] text-[#C92F28] border-none focus:outline-none font-roboto lg:text-[18px]"
+          defaultValue="uzb"
+        >
+          <option value="rus">Rus</option>
+          <option value="uzb">Uzb</option>
+        </select>
+      </div>
     </div>
   );
 }
